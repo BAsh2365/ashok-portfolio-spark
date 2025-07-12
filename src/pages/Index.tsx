50c365c50480c3f4fc7901e4c804ac82d18e32de
Bhargav Ashok <bhargav.ashok2023@gmail.com>
2025-07-12 00:48:29 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -113,7 +113,6 @@ const Index = () => {
         "Supervised Learning",
         "Unsupervised Learning",
         "Contrastive Learning",
-        "Natural Language Processing",
         "Data Retrieval & Fine-Tuning"
       ]
     },
@@ -150,7 +149,7 @@ const Index = () => {
       highlights: [
         "Crafted an ETL pipeline in Node.js to migrate and transform PostgreSQL data into Neo4j",
         "Improved Neo4j query times by 53% on average using optimization techniques",
-        "Conducted end-to-end tests for larger instances (14 second improved query for 1000-part BOM)"
+        "Conducted end-to-end tests for larger instances (14-second improved query for 1000-part BOM)"
       ]
     },
     {
